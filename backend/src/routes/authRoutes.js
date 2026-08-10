a85cd73db3694ff3d21c0c1ddc1b44e5108366ff
dipishatimsina <dipishatimsina1@gmail.com>
2026-08-10 19:43:36 +0545

    feat: add protected route to retrieve user information

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+// Test protected route
+router.get('/me', protect, (req, res) => {
+  res.json(req.user);
+});
 
 module.exports = router;
